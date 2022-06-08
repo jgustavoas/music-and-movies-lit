4d600461be778d20a7f92ced1ce550351c60b1d9
Gustavo <jgustavoas@gmail.com>
2022-06-08 20:18:43 -0300

    Generating model and migration for table "genres"

---
+++ api/migrations/20220608224959-create-genre.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('genres', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      genre: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('genres');
+  },
+};
---
+++ api/models/genre.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class genre extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  genre.init(
+    {
+      genre: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'genre',
+    }
+  );
+  return genre;
+};
