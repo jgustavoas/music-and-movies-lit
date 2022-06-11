97523d72c8321d8889a37f29a7a599f9c4f2dfe5
Gustavo <jgustavoas@gmail.com>
2022-06-10 22:04:08 -0300

    Responding with json

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('Welcome to Express');
+  res.json({ title: 'Express', text: 'Welcome to Express' });
 });
 
 module.exports = router;
