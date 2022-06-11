5edab17a4e4f22067084e89fe0749b018330612c
Gustavo <jgustavoas@gmail.com>
2022-06-10 21:58:18 -0300

    Deleting codes related to "public" folder

--- api/app.js
+++ api/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -12,7 +11,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
