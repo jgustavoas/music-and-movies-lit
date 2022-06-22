9c82e9f6f8a231132490182c00295c930c7541f7
Gustavo <jgustavoas@gmail.com>
2022-06-22 18:39:48 -0300

    Using "this" keyword instead of "models.artists"

--- api/models/artists.js
+++ api/models/artists.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.artists.belongsTo(models.genres, {
+      this.belongsTo(models.genres, {
         foreignKey: 'genre_id',
       });
     }
