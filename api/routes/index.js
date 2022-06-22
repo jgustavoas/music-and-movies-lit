062a7fd015c368fbd340de6e79ef81cf812e0865
Gustavo <jgustavoas@gmail.com>
2022-06-22 19:17:53 -0300

    Route for "artists" model

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const models = require('../models');
 
 const router = express.Router();
 
-/* GET home page. */
+/* home page */
 router.get('/', (req, res, next) => {
-  res.json({ title: 'Express', text: 'Welcome to Express' });
+  res.json({
+    title: 'Music and Movies Lit App',
+    text: 'Welcome to Music and Movies Lit App',
+  });
+});
+
+/* models page */
+router.get('/artists', async (req, res, next) => {
+  try {
+    const data = await models.artists.findAll({
+      attributes: ['id', 'artist', 'genre_id', 'created_at', 'updated_at'],
+    });
+    res.json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
