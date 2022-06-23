5040666f84ab90ece4c0de775a5201c8bbad565d
Gustavo <jgustavoas@gmail.com>
2022-06-23 20:49:38 -0300

    Seed file for "tracks" table

---
+++ api/seeders/20220623233626-tracks.js
@@ -0,0 +1,127 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   track: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'tracks',
+      [
+        {
+          track: 'Once',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Even Flow',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Alive',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Why Go',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Black',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Jeremy',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Oceans',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Porch',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Garden',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Deep',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          track: 'Release',
+          artist_id: 18,
+          album_id: 13,
+          genre_id: 1,
+          movie_id: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
