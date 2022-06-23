a11e71c22df49009f154e857d02f7617e03e41c3
Gustavo <jgustavoas@gmail.com>
2022-06-22 21:00:07 -0300

    Updating the order of module dependencies

--- api/server.js
+++ api/server.js
@@ -4,28 +4,24 @@
 /**
  * Module dependencies.
  */
-
-var app = require('./app');
-var debug = require('debug')('api:server');
 var http = require('http');
+var debug = require('debug')('api:server');
+var app = require('./app');
 
 /**
  * Get port from environment and store in Express.
  */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
-
 var server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
@@ -33,7 +29,6 @@ server.on('listening', onListening);
 /**
  * Normalize a port into a number, string, or false.
  */
-
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
@@ -53,7 +48,6 @@ function normalizePort(val) {
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
@@ -79,7 +73,6 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
